ae0cecdb8dc359d18ff41173dea70e0fe3ddbd44
Anderson <marquesandersonds@gmail.com>
2022-09-12 12:09:21 -0300

    V1

---
+++ index.js
@@ -0,0 +1,80 @@
+let rn0 = document.querySelector("#rn0");
+let rn1 = document.querySelector("#rn1");
+let rn2 = document.querySelector("#rn2");
+let rn3 = document.querySelector("#rn3");
+let rn4 = document.querySelector("#rn4");
+let rn5 = document.querySelector("#rn5");
+let rn6 = document.querySelector("#rn6");
+let rn7 = document.querySelector("#rn7");
+let rn8 = document.querySelector("#rn8");
+let rn9 = document.querySelector("#rn9");
+
+
+conta()
+function conta (){
+    let rn0 = 0;
+    let rn1 = 0;
+    let rn2 = 0;
+    let rn3 = 0;
+    let rn4 = 0;
+    let rn5 = 0;
+    let rn6 = 0;
+    let rn7 = 0;
+    let rn8 = 0;
+    let rn9 = 0;
+}
+
+let count0 = 0;
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
+let count5 = 0;
+let count6 = 0;
+let count7 = 0;
+let count8 = 0;
+let count9 = 0;
+
+
+function acrescentar0(){
+    count0++
+    rn0.value = count0
+}
+function acrescentar1(){
+    count1++
+    rn1.value = count1
+}
+function acrescentar2(){
+    count2++
+    rn2.value = count2
+}
+function acrescentar3(){
+    count3++
+    rn3.value = count3
+}
+function acrescentar4(){
+    count4++
+    rn4.value = count4
+}
+function acrescentar5(){
+    count5++
+    rn5.value = count5
+}
+function acrescentar6(){
+    count6++
+    rn6.value = count6
+}
+function acrescentar7(){
+    count7++
+    rn7.value = count7
+}
+function acrescentar8(){
+    count8++
+    rn8.value = count8
+}
+function acrescentar9(){
+    count9++
+    rn9.value = count9
+}
+
+let listaDinamica = []
